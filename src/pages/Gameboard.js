c6f2a72346c237afb1d42046e43ab058db21322b
AmteurPotion <1k0j2i4@gmail.com>
2021-10-10 23:39:18 +0900

    컴포넌트 정리 및 게임 상태창 추가

--- src/pages/Gameboard.js
+++ src/pages/Gameboard.js
@@ -4,30 +4,7 @@ import { Omok } from "../game";
 import LioWebRTC from 'liowebrtc';
 import './Gameboard.css';
 
-const textureMap = {
-    "none": "img/stone-none.png",
-    "black": "img/stone-black.png"
-};
-
-function Board({board, place = (e, x, y) => { alert(x + " / " + y); }}){
-        if(board != null && typeof board.board == typeof Function) {
-            return (
-                <div className="board">
-                    {board.board().map((array, y) => (<div className="line" key={y}>
-                        {array.map((val, x) => (
-                            <img 
-                                onClick={(e) => place(e, x, y)} alt="cell" key={y * array.length + x} 
-                                src={textureMap[val]} width="3%"
-                            />
-                        ))}
-                        <br/>
-                    </div>))}
-                </div>
-            );
-        } else {
-            return (<div>loading...</div>)
-        }
-    };
+//const publish = false; (publish ? "./" : "") + 
 
 const Gameboard = () => {
     const history = useHistory();
@@ -45,8 +22,13 @@ const Gameboard = () => {
         // Game loading
         if(state != null) {
             if(state.state === "start") {
-                changeState({...state, state: "progress", status: new Omok(state.boardWidth, state.boardHeight)});
-                console.log(state);
+                changeState({
+                    ...state, 
+                    state: "progress",
+                    gameCondition: "게임 시작 대기중",
+                    status: new Omok(state.boardWidth, state.boardHeight)
+                });
+                localStorage.setItem("cellSize", "32px");
             }
         } else {
             alert("올바르지 않은 접근으로 인해 첫 화면으로 이동됩니다.");
@@ -62,15 +44,13 @@ const Gameboard = () => {
     } , [state, changeState, history]);
     
     // Sync
-    
-    if(state != null) {
-        if(state.status == null) {
-            changeState({...state, status: new Omok(state.boardWidth, state.boardHeight)})
-        } else if(typeof state.status != typeof Omok) {
-            if(!state.status.hasOwnProperty("board")) {
-                let data = Omok.loadData(state.status);
-                changeState({...state, status: data})
-            }
+
+    if(state.status == null) {
+        changeState({...state, status: new Omok(state.boardWidth, state.boardHeight)})
+    } else if(typeof state.status != typeof Omok) {
+        if(!state.status.hasOwnProperty("board")) {
+            let data = Omok.loadData(state.status);
+            changeState({...state, status: data})
         }
     }
     
@@ -89,11 +69,75 @@ const Gameboard = () => {
         
     };
 
+    // game
 
+    function Board({board, place = (e, x, y) => { alert(x + " / " + y); }}){
+    const cellSize = localStorage.getItem("cellSize") != null ? localStorage.getItem("cellSize") : "32px", cellIntSize = parseInt(cellSize);
+        if(board != null && typeof board.board == typeof Function) {
+            return (
+                <div className="board">
+                    {board.board().map((array, y) => (<div className="line" key={y}>
+                        {array.map((val, x) => (
+                            <svg
+                                alt="cell" key={y * array.length + x}
+                                onClick={(e) => {place(e, x, y); console.log(x + " / " + y)}}
+                                width={cellSize} height={cellSize}
+                            >
+                                <rect width={cellSize} height={cellSize} fill="sandybrown"/>
+                                <line x1={cellIntSize / 2} y1={0} x2={cellIntSize / 2} y2={cellIntSize} stroke="black" strokeWidth={cellIntSize / 10}/>
+                                <line x1={0} y1={cellIntSize / 2} x2={cellIntSize} y2={cellIntSize / 2} stroke="black" strokeWidth={cellIntSize / 10}/>
+                                <circle cx={cellIntSize / 2} cy={cellIntSize / 2} r={cellIntSize / 3} fill={val} />
+                                {/* 외곽선 */}
+                                {(() => x === 0 || x === array.length - 1 ? 
+                                (<line x1={x === 0 ? 0 : cellIntSize} y1={0} x2={x === 0 ? 0 : cellIntSize} y2={cellIntSize} stroke="black" strokeWidth={cellIntSize / 10} />) : "")()}
+                                {(() => y === 0 || y === array.length - 1 ? 
+                                (<line x1={0} y1={y === 0 ? 0 : cellIntSize} x2={cellIntSize} y2={y === 0 ? 0 : cellIntSize} stroke="black" strokeWidth={cellIntSize / 10} />) : "")()}
+                            </svg>
+                        ))}
+                        <br/>
+                    </div>))}
+                </div>
+            );
+        } else {
+            return (<div>loading...</div>)
+        }
+    };
+
+    // game info
+    
+    function SpecList({props}) {
+        return (
+            <div className="speclist" >
+
+            </div>
+        );
+    }
+
+    function Timer({second = 0, runner, pause = false}) {
+        return (
+            <>
+                개발 예정
+            </>
+        );
+    }
 
     return (
         <div className="gameboard">
             {(() => state != null ? <Board board={state.status} place={placeEvent}/> : null)()}
+            <br/>
+            <div className="information">
+                <h2 className="title"> 상태창 </h2>
+                게임 진행 상태 : {state.gameCondition} <br/>
+                타이머 : <Timer /> <br/>
+                <button>항복</button>
+                <SpecList/> <br/>
+            </div>
+            {/* fab */}
+            <div className="circle" style={{
+                position: "fixed", 
+                backgroundColor: "pink", 
+                right: "3%", bottom: "3%"
+                }}> a</div>
         </div>
     );
 }
